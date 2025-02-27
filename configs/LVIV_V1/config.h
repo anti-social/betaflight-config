625760a15135cafe25e46f0e747739420cfeeb9e
Alexander Koval <kovalidis@gmail.com>
2025-02-27 18:43:19 +0200

    Add LVIV_V1

---
+++ configs/LVIV_V1/config.h
@@ -0,0 +1,111 @@
+/*
+ * This file is part of Betaflight.
+ *
+ * Betaflight is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Betaflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define FC_TARGET_MCU     STM32F405
+
+#define BOARD_NAME        MODULE149V1
+#define MANUFACTURER_ID   LVIV
+
+#define USE_GYRO
+#define USE_ACC
+#define USE_ACCGYRO_BMI270
+#define USE_MAX7456
+#define USE_FLASH
+#define USE_FLASH_W25Q128FV
+#define USE_BARO
+#define USE_BARO_DPS310
+#define USE_SERVO
+#define USE_MAG
+
+#define BEEPER_PIN 			 PB4
+#define MOTOR1_PIN           PB0
+#define MOTOR2_PIN           PB1
+#define MOTOR3_PIN           PA3
+#define MOTOR4_PIN           PA2
+#define SERVO1_PIN           PA1
+#define SERVO2_PIN           PA8
+#define UART1_TX_PIN         PA9
+#define UART6_TX_PIN         PC6
+#define UART1_RX_PIN         PA10
+#define UART6_RX_PIN         PC7
+#define I2C2_SCL_PIN         PB10
+#define I2C2_SDA_PIN         PB11
+#define LED0_PIN             PB5
+#define SOFTSERIAL1_TX_PIN   PB14
+#define SPI1_SCK_PIN         PA5
+#define SPI3_SCK_PIN		 PC10
+#define SPI1_SDI_PIN         PA6
+#define SPI3_SDI_PIN		 PC11
+#define SPI1_SDO_PIN         PA7
+#define SPI3_SDO_PIN		 PC12
+#define ADC_VBAT_PIN         PC2
+#define ADC_CURR_PIN         PC1
+#define FLASH_CS_PIN         PB3
+#define MAX7456_SPI_CS_PIN   PA15
+#define GYRO_1_EXTI_PIN      PC4
+#define GYRO_1_CS_PIN        PA4
+#define USB_DETECT_PIN       PC5
+
+
+#define TIMER_PIN_MAPPING \
+    TIMER_PIN_MAP( 0, PB0,  1,  0) \
+    TIMER_PIN_MAP( 1, PB1,  1,  0) \
+    TIMER_PIN_MAP( 2, PA3,  2,  0) \
+    TIMER_PIN_MAP( 3, PA2,  2,  0) \
+    TIMER_PIN_MAP( 4, PA1,  2,  0) \
+    TIMER_PIN_MAP( 5, PA8,  1,  0) \
+    TIMER_PIN_MAP( 6, PB14, 3,  -1)
+		
+
+#define SPI2_TX_DMA_OPT     0
+#define ADC1_DMA_OPT        0
+
+#define MAG_I2C_INSTANCE I2CDEV_2
+#define BARO_I2C_INSTANCE I2CDEV_2
+
+#define DEFAULT_BLACKBOX_DEVICE     BLACKBOX_DEVICE_FLASH
+#define FLASH_SPI_INSTANCE SPI3
+
+#define DEFAULT_DSHOT_BURST DSHOT_DMAR_ON
+
+#define BEEPER_INVERTED
+#define SYSTEM_HSE_MHZ 8
+#define MAX7456_SPI_INSTANCE SPI3
+#define DASHBOARD_I2C_INSTANCE I2CDEV_2
+#define PINIO1_CONFIG 129
+#define PINIO1_BOX 0
+#define GYRO_1_SPI_INSTANCE SPI1
+#define USE_SOFTSERIAL
+// #define USE_VTX
+
+#define DEFAULT_CURRENT_METER_SOURCE CURRENT_METER_ADC
+#define DEFAULT_VOLTAGE_METER_SOURCE VOLTAGE_METER_ADC
+#define DEFAULT_VOLTAGE_METER_SCALE 109
+#define DEFAULT_CURRENT_METER_SCALE 263
+
+
+#define GYRO_1_ALIGN CW180_DEG
+#define GYRO_1_ALIGN_YAW 1800
+
+#define SERIALRX_UART           SERIAL_PORT_USART1
+#define VTX_SMARTAUDIO_UART     SERIAL_PORT_SOFTSERIAL1
